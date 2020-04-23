dc208207f59577f8039532ef0478d6a5df1a29d6
Nilotpal Dhar <nilotpaldhar1997@gmail.com>
2020-04-23 21:48:41 +0530

    Initial Commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Youtube</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+
+    <div id="pageContainer">
+        <div id="mastHeadContainer"></div>
+        <div id="sideNavContainer"></div>
+        <div id="mainSectionContainer"></div>
+    </div>
+
+</body>
+</html>
